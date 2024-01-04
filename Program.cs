5b93a43d38350dff53434d3aeaf63d63281a2aea
NAVIN KUMAR <87271759+NavinKumar28@users.noreply.github.com>
2024-01-04 15:54:41 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDataViewer
+{
+    using System;
+    using System.IO;
+
+    class Program
+    {
+        static void Main()
+        {
+          
+            string filePath = "C:\\Users\\Navin\\source\\repos\\students.txt";
+
+            if (File.Exists(filePath))
+            {
+                string[] lines = File.ReadAllLines(filePath);
+
+                // Display student data
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            else
+            {
+                Console.WriteLine("File not found!");
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+}
